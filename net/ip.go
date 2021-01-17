fd026d2c9d68c143e42b475f68b21819f2c0bebb
vysion <77179954+vysiondev@users.noreply.github.com>
2021-01-16 23:40:02 -0800

    removed check for x-forwarded-for

--- net/ip.go
+++ net/ip.go
@@ -5,11 +5,7 @@ import "github.com/valyala/fasthttp"
 // GetIP get the IP from the header.
 // It will try to fetch the client IP forwarded from Cloudflare.
 func GetIP(ctx *fasthttp.RequestCtx) string {
-	forwardedIP := ctx.Request.Header.Peek("X-Forwarded-For")
-	if len(forwardedIP) != 0 {
-		return string(forwardedIP)
-	}
-	forwardedIP = ctx.Request.Header.Peek("X-Real-IP")
+	forwardedIP := ctx.Request.Header.Peek("X-Real-IP")
 	if len(forwardedIP) != 0 {
 		return string(forwardedIP)
 	}
